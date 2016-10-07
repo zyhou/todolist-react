e6271854e4d147e721213f9f9949f4af33d2562f
Maxime <richard.maxime9@gmail.com>
2016-10-07 07:43:13 +0200

    Mise en place css et html de base

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,10 +9,21 @@ export default class TodoList extends Component {
 
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-        Salut
+      <div className="block">
+
+        <div className="info-date">
+          <div className="date">25 Jul 2016</div>
+          <div className="day">Monday</div>
         </div>
+
+        <div className="list">
+          <ul>
+            <li><a href="#" className="list-item">Apprendre plus sur React</a><input type="checkbox" id="one" checked/><label for="one"></label></li>
+            <li><a href="#">Faire une API en .NET Core</a><input type="checkbox" id="two"/><label for="two"></label></li>
+          </ul>
+        </div>
+
+        <a href="#" className="btn"></a>
       </div>
     );
   }
